79b6d20aa3fcbb102f6dba6e338cbcd6aba0d952
Rémi Bovard <remi.bovard@nyon.ch>
2017-06-02 16:48:32 +0200

    Do release 2.18.2

--- SettingsManager/settingsmanager.py
+++ SettingsManager/settingsmanager.py
@@ -31,7 +31,7 @@ import os.path
 
 class SettingsManager:
 
-    PLUGIN_VERSION = u"2.18.1"
+    PLUGIN_VERSION = u"2.18.2"
     GEODATA_PATH = os.path.normpath("S:\\")
     PROJECTION = u"EPSG:21781"
 
